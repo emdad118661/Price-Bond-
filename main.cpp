a8c66ac23d593ff3f6378bfe4268e4afc23ae60c
emdad118661 <63422847+emdad118661@users.noreply.github.com>
2021-01-04 17:08:48 +0600

    main,cpp

---
+++ main.cpp
@@ -0,0 +1,330 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "enter your number:" << endl;
+    cin>>a;
+    if(a>=577821 && a<=577900){
+        cout<<"congratulations!0010"<<endl;
+    }
+     else if(a>=158561 && a<=158600){
+        cout<<"congratulations!0013"<<endl;
+    }
+      else if(a>=3601 && a<=3604){
+        cout<<"congratulations!0003"<<endl;
+    }
+    else if(a>=593001 && a<=593004){
+        cout<<"congratulations!0003"<<endl;
+    }
+    else if(a==509161 || a==534243 || a==567022 || a==567023){
+        cout<<"congratulations!0003"<<endl;
+    }
+    else if(a>=634701 && a<=634705){
+        cout<<"congratulations!0003"<<endl;
+    }
+    else if(a==634720 || a==634721){
+        cout<<"congratulations!0003"<<endl;
+    }
+    else if(a==605831 || a==613348 || a==624105){
+        cout<<"congratulations!0003"<<endl;
+    }
+    else if(a>=692049 && a<=692056){
+        cout<<"congratulations!0003"<<endl;
+    }
+    else if(a>=720502 && a<=720503){
+        cout<<"congratulations!0003"<<endl;
+    }
+    else if(a>=720524 && a<=720540){
+        cout<<"congratulations!0003"<<endl;
+    }
+    else if(a>=738761 && a<=738766){
+        cout<<"congratulations!0003"<<endl;
+    }
+    else if(a==740826 || a==744992){
+        cout<<"congratulations!0003"<<endl;
+    }
+    else if(a>=745975 && a<=745977){
+        cout<<"congratulations!0003"<<endl;
+    }
+    else if(a==747352 || a==748394 || a==750988 || a==752212 || a==752213 || a==758606){
+        cout<<"congratulations!0003"<<endl;
+    }
+    else if(a>=760818 && a<=760822){
+        cout<<"congratulations!0003"<<endl;
+    }
+    else if(a==761669 || a==761715 || a==762739 || a==773070){
+        cout<<"congratulations!0003"<<endl;
+    }
+    else if(a>=773051 && a<=773065){
+        cout<<"congratulations!0003"<<endl;
+    }
+    else if(a==793836 || a==837788 || a==891887 || a==960549 || a==997181 || a==997182 || a==651700 || a==672483 || a==677880 || a==691329 || a==691331){
+        cout<<"congratulations!0003"<<endl;
+    }
+    else if(a>=656831 && a<=656860 || a==658360){
+        cout<<"congratulations, 0001!"<<endl;
+    }
+
+    else if(a>=780521 && a<=780600){
+        cout<<"congratulations!0009"<<endl;
+    }
+    else if(a>=924856 && a<=924868){
+        cout<<"congratulations!0012"<<endl;
+    }
+    else if(a==855809 || a==855810 || a==855847 || a==855848 || a==729350){
+        cout<<"congratulations!0012"<<endl;
+    }
+    else if(a>=729280 && a<=729282){
+        cout<<"congratulations!0012"<<endl;
+    }
+    else if(a>=729285 && a<=729288){
+        cout<<"congratulations!0012"<<endl;
+    }
+    else if(a>=176841 && a<=176849){
+        cout<<"congratulations!0012"<<endl;
+    }
+    else if(a>=206641 && a<=206645){
+        cout<<"congratulations!0012"<<endl;
+    }
+    else if(a==809786 || a==809778 || a==809779 || a==809784 || a==809785 || a==549074 || a==549075 || a==549076 || a==924853 || a==924854 || a==924855 || a==855870 || a==855872){
+        cout<<"congratulations!0012"<<endl;
+    }
+    else if(a>=869507 && a<=869511){
+        cout<<"congratulations!0012"<<endl;
+    }
+    else if(a>=865218 && a<=865222){
+        cout<<"congratulations!0012"<<endl;
+    }
+    else if(a>=893556 && a<=893562){
+        cout<<"congratulations!0012"<<endl;
+    }
+    else if(a>=776990 && a<=776994){
+        cout<<"congratulations!0012"<<endl;
+    }
+    else if(a==776999){
+        cout<<"congratulations!0012"<<endl;
+    }
+    else if(a>=51006 && a<=51010){
+        cout<<"congratulations!0012"<<endl;
+    }
+    else if(a>=271011 && a<=271015){
+        cout<<"congratulations!0012"<<endl;
+    }
+    else if(a>=417892 && a<=417894){
+        cout<<"congratulations!0012"<<endl;
+    }
+    else if(a>=40528 && a<=40532){
+        cout<<"congratulations!0012"<<endl;
+    }
+    else if(a>=3601 && a<=3604){
+        cout<<"congratulations!"<<endl;
+    }
+    else if(a>=217272 && a<=217274){
+        cout<<"congratulations!0012"<<endl;
+    }
+    else if(a>=839761 && a<=839763){
+        cout<<"congratulations!0012"<<endl;
+    }
+    else if(a>=648146 && a<=648147){
+        cout<<"congratulations!0012"<<endl;
+    }
+    else if(a==969623 || a==370963){
+        cout<<"congratulations!0011"<<endl;
+    }
+    else if(a>=100157 && a<=100159){
+        cout<<"congratulations!0011"<<endl;
+    }
+    else if(a>=897184 && a<=897186){
+        cout<<"congratulations!0011"<<endl;
+    }
+    else if(a>=897195 && a<=897197){
+        cout<<"congratulations!0011"<<endl;
+    }
+    else if(a>=897542 && a<=897544){
+        cout<<"congratulations!0011"<<endl;
+    }
+    else if(a==897270 || a==897286 || a==897318 || a==897443){
+        cout<<"congratulations!0011"<<endl;
+    }
+    else if(a==896831 || a==896835 || a==896836 || a==896856 || a==897072 || a==897073 || a==897140 || a==816891 || a==816913 || a==816932 || a==817092 || a==817093){
+        cout<<"congratulations!0011"<<endl;
+    }
+    else if(a>=817112 && a<=817116){
+        cout<<"congratulations!0011"<<endl;
+    }
+    else if(a>=817220 && a<=817224){
+        cout<<"congratulations!0011"<<endl;
+    }
+    else if(a==817233 || a==817234 || a==817136 || a==817137 || a==817173 || a==905703){
+        cout<<"congratulations!0011"<<endl;
+    }
+       else if(a>=899381 && a<=899385){
+        cout<<"congratulations!0011"<<endl;
+    }
+       else if(a>=899387 && a<=899400){
+        cout<<"congratulations!0011"<<endl;
+    }
+    else if(a>=720541 && a<=720580){
+        cout<<"congratulations!0011"<<endl;
+    }
+    else if(a>=658321 && a<=658360){
+        cout<<"congratulations!"<<endl;
+    }
+    else if(a>=648146 && a<=648147){
+        cout<<"congratulations!"<<endl;
+    }
+    else if(a>=720101 && a<=720160){
+        cout<<"congratulations!0008"<<endl;
+    }
+    else if(a==208147 || a==208148 || a==208166){
+        cout<<"congratulations!0008"<<endl;
+    }
+    else if(a>=208159 && a<=208163){
+        cout<<"congratulations!0008"<<endl;
+    }
+    else if(a==177451 || a==177657 || a==524044 || a==524045 || a==992326 || a==992328 || a==557219 || a==301617 || a==319079){
+        cout<<"congratulations!0008"<<endl;
+    }
+    else if(a>=70411 && a<=70430){
+        cout<<"congratulations!0008"<<endl;
+    }
+    else if(a==344148 || a==373475 || a==100435){
+        cout<<"congratulations!0008"<<endl;
+    }
+    else if(a>=104178 && a<=104197){
+        cout<<"congratulations!0010"<<endl; //3 missing 197
+    }
+    else if(a==887155 || a==607384 || a==689856 || a==872976 || a==872968 || a==872978 || a==872967|| a==464810 || a==529639 || a==520069 || a==529644 || a==534914 || a==189152 || a==176738 || a==126411 || a==176066 || a==954420 || a==969621 || a==969622 || a==241528 || a==748533 || a==818104 || a==863694 || a==60270 || a==553092 || a==553093 || a==553094 || a==553095){
+        cout<<"congratulations!0013"<<endl;
+    }
+    else if(a>=553076 && a<=553082){
+        cout<<"congratulations!0013"<<endl;
+    }
+    else if(a>=104198 && a<=104200){
+        cout<<"congratulations!0013"<<endl;
+    }
+    else if(a>=706561 && a<=706570){
+        cout<<"congratulations!0013"<<endl;
+    }
+    else if(a==55738 || a==55739 || a==94802 || a==93890 || a==99683 || a==104121){
+        cout<<"congratulations!0013"<<endl;
+    }
+    else if(a>=569001 && a<=569040){
+        cout<<"congratulations!, 0001"<<endl;//0001
+    }
+    else if(a>=307983 && a<=307991){
+        cout<<"congratulations! 0001"<<endl;//0001
+    }
+    else if(a==1983 || a==1984 || a==1985 || a==1991 || a==261451 || a==461556 || a==102338 || a==36550 || a==382430 || a==361761){
+        cout<<"congratulations! 0001"<<endl;//0001
+    }
+    else if(a==320661 || a==317973 || a==870333 || a==87006 || a==802089 || a==143486 || a==143488 || a==972400 || a==222396 || a==734145 || a==735383 || a==735385){
+        cout<<"congratulations! 0001"<<endl;//0001
+    }
+    else if(a>=60779 && a<=60799){
+        cout<<"congratulations!0002"<<endl; //0002
+    }
+    else if(a>=583458 && a<=583466){
+        cout<<"congratulations!0002"<<endl; //0002
+    }
+    else if(a>=731310 && a<=731311){
+        cout<<"congratulations!"<<endl;
+    }
+    else if(a>=732997 && a<=732999){
+        cout<<"congratulations!"<<endl;
+    }
+    else if(a==732413 || a==733173 || a==733174 || a==733583 || a==735212 || a==7035720){
+        cout<<"congratulations!"<<endl;
+    }
+    else if(a==735721 || a==735778 || a==735912 || a==738042 || a==738321 || a==739375 || a==739669){
+        cout<<"congratulations!"<<endl;
+    }
+    else if(a==739752 || a==800818 || a==950354 || a==950355 || a==463621){
+        cout<<"congratulations!"<<endl;
+    }
+    else if(a>=463621 && a<=463623){
+        cout<<"congratulations!"<<endl;
+    }
+    else if(a>=756388 && a<=756390){
+        cout<<"congratulations!"<<endl;
+    }
+    else if(a>=503501 && a<=503600){
+        cout<<"congratulations!0014"<<endl;
+    }
+    else if(a>=563921 && a<=563940){
+        cout<<"congratulations!0014"<<endl;
+    }
+    else if(a==427249 || a==427244){
+        cout<<"congratulations!"<<endl;
+    }
+    else if(a>=265201 && a<=265240){
+        cout<<"congratulations!0014"<<endl;
+    }
+      else if(a>=576551 && a<=576600){
+        cout<<"congratulations!0004"<<endl;
+    }
+      else if(a>=546401 && a<=546420){
+        cout<<"congratulations!0005"<<endl;
+      }
+        else if(a>=408101 && a<=408140){
+        cout<<"congratulations!0005"<<endl;
+    }
+      else if(a>=872801 && a<=872900){
+        cout<<"congratulations!0005"<<endl;
+    }
+      else if(a>=542101 && a<=542200){
+        cout<<"congratulations!0006"<<endl;
+    }
+      else if(a>=546061 && a<=546080){
+        cout<<"congratulations!0006"<<endl;
+    }
+      else if(a>=918192 && a<=918195 || a==677748){
+        cout<<"congratulations!0007"<<endl;
+    }
+      else if(a>=705061 && a<=705065){
+        cout<<"congratulations!0007"<<endl;
+    }
+       else if(a==509372 || a==509373 || a==509376 || a==509308 || a==509473 || a==901759 || a==702091 || a==702376 || a==500319 || a==500330 || a==500834 || a==500851 || a==500852 || a==300101 || a==800774 || a==801652 || a==801712 || a==801713 || a==802634 || a==602479 || a==603716 || a==603811 || a==603812 || a==603813 || a==400112 || a==400113 || a==400596 || a==400631 || a==400632){
+        cout<<"congratulations!0007"<<endl;
+    }
+       else if(a>=310020 && a<=310024){
+        cout<<"congratulations!0007"<<endl;
+    }
+       else if(a==116951 || a==116952 || a==117842 || a==118103 || a==118159 || a==200579 || a==1290 || a==1714 || a==1715 || a==2205 || a==2794 || a==607566 || a==417405 || a==417617 || a==417619 || a==417620 || a==417640 || a==218256 || a==218578 || a==218579 || a==219033 || a==219046 ){
+        cout<<"congratulations!0007"<<endl;
+    }
+       else if(a>=821626 && a<=821630){
+        cout<<"congratulations!0007"<<endl;
+    }
+       else if(a>=608159 && a<=608162){
+        cout<<"congratulations!0007"<<endl;
+    }
+       else if(a>=18455 && a<=18459){
+        cout<<"congratulations!0007"<<endl;
+    }
+       else if(a>=903526 && a<=903529){
+        cout<<"congratulations!0007"<<endl;
+    }
+       else if(a>=700510 && a<=700512){
+        cout<<"congratulations!0007"<<endl;
+    }
+       else if(a>=200492 && a<=200495){
+        cout<<"congratulations!0007"<<endl;
+    }
+       else if(a>=100876 && a<=100880){
+        cout<<"congratulations!0007"<<endl;
+    }
+       else if(a>=300105 && a<=300108){
+        cout<<"congratulations!0007"<<endl;
+    }
+    else if(a==176062 || a==176270 || a==176386 || a==176479 || a==176480 || a==176489 || a==176528 || a==176630 || a==176814 || a==176815 || a==167950 || a==107807 || a==107808 || a==108198 || a==86844 || a==86848 || a==86854 || a==86856 || a==86858 || a==86860){
+        cout<<"congratulations!0009"<<endl;
+    }
+    else{
+        cout<<"sorry! number is unavailable"<<endl;
+    }
+    return 0;
+}
